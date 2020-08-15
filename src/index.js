c5d4224c2702dd60c62e322c51a067e4bc44599c
sandip satyal <sandipsatyal25@gmail.com>
2020-08-15 19:52:28 +0545

    [add] bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap-css-only/css/bootstrap.css'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
