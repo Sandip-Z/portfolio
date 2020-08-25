9adf81a3df590d52c9125b293740b1be38d0ba97
sandip satyal <sandipsatyal25@gmail.com>
2020-08-25 17:25:50 +0545

    prettify

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,46 +1,71 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import ChasingShadow from '../hoc/chasingShadow'
+import React from "react";
+import { Link } from "react-router-dom";
+import ChasingShadow from "../hoc/chasingShadow";
 
-const Box = ({description, technology, code, live, title, detail}) => {
-    const getTechnology = () => {
-        if(technology && technology.length){
-            const kbdRendered = technology.map(tech => {
-                return <small key={tech} className="mx-1 font-weight-lighter">{tech}</small>
-            })
-            return kbdRendered
-        }else{
-            return <small>Technology n/a</small>
-        }
+const Box = ({ description, technology, code, live, title, detail }) => {
+  const getTechnology = () => {
+    if (technology && technology.length) {
+      const kbdRendered = technology.map((tech) => {
+        return (
+          <small key={tech} className="mx-1 font-weight-lighter">
+            {tech}
+          </small>
+        );
+      });
+      return kbdRendered;
+    } else {
+      return <small>Technology n/a</small>;
     }
-    return <div>
-        <div>
-            <h3 className="mb-0">{title || 'Title'}</h3>
-            <p className="mt-2">
-                {getTechnology()}
-            </p>
-            <p className="text-overflow-ellipsis h-100 work-description">{description || 'Description n/a'}</p>
-        </div>
-        <div className="d-flex justify-content-start mt-3 font-weight-light work-footer">
-            {live && <div title="View Live">
-                <i className="fa fa-eye" aria-hidden="true"></i>
-                <a href={live} className="text-decoration-none ml-1 work-footer" target="_blank">Live</a>
-            </div>}
-            {
-                code && <div className="mx-3" title="View Code">
-                <i className="fa fa-code" aria-hidden="true"></i>
-                <a href={code} className="text-decoration-none ml-1 work-footer" target="_blank">Code</a>
+  };
+  return (
+    <div className="d-flex flex-column">
+      <div>
+        <h3 className="mb-0">{title || "Title"}</h3>
+        <p className="mt-2">{getTechnology()}</p>
+        <p className="text-overflow-ellipsis h-100 work-description">
+          {description || "Description n/a"}
+        </p>
+      </div>
+      <div className="font-weight-light work-footer mt-auto">
+        <div className="d-flex justify-content-start">
+          {live && (
+            <div title="Live">
+              <i className="fa fa-eye" aria-hidden="true"></i>
+              <a
+                href={live}
+                className="text-decoration-none ml-1 work-footer"
+                target="_blank"
+              >
+                Live
+              </a>
             </div>
-            }
-            {
-                detail && <div className="ml-auto" title="Details of the project">
-                    <Link to={detail} className="text-decoration-none mr-2 work-footer">
-                        <i class="fa fa-info" aria-hidden="true"></i>
-                    </Link>
-                </div>
-            }
+          )}
+          {code && (
+            <div className="mx-3" title="Code">
+              <i className="fa fa-code" aria-hidden="true"></i>
+              <a
+                href={code}
+                className="text-decoration-none ml-1 work-footer"
+                target="_blank"
+              >
+                Code
+              </a>
+            </div>
+          )}
+          {detail && (
+            <div className="ml-auto" title="Detail">
+              <Link
+                to={detail}
+                className="text-decoration-none mr-2 work-footer"
+              >
+                <i class="fa fa-info" aria-hidden="true"></i>
+              </Link>
+            </div>
+          )}
         </div>
+      </div>
     </div>
-}
+  );
+};
 
-export default ChasingShadow(Box)
+export default ChasingShadow(Box);
--- src/page/Work.js
+++ src/page/Work.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import Box from '../components/Box'
+import React from "react";
+import Box from "../components/Box";
 const Work = () => {
-    return <div className="text-white d-flex flex-wrap mt-5">
-        <Box 
-            technology={['React', 'HTML', 'CSS']}
-            title="Portfolio"
-            description="My portfolio website."
-            code="https://github.com/Sandip-Z/portfolio"
-            live="http://www.sandipsatyal.com.np"
-        />
-        <Box />
-        <Box />
-        <Box 
-            description="This is a dummy description"
-            title="I am a title"
-            technology={['node', 'express', 'ejs', 'mysql', 'react', 'javascript']}
-            code="https://something.com"
-            live="s"
-            detail
-        />
+  return (
+    <div className="text-white d-flex flex-wrap mt-5">
+      <Box
+        technology={["React", "HTML", "CSS"]}
+        title="Portfolio"
+        description="My portfolio website."
+        code="https://github.com/Sandip-Z/portfolio"
+        live="http://www.sandipsatyal.com.np"
+      />
+      <Box
+        description="Helps check your website's accessibility, broken links etc"
+        title="Site Auditor"
+        technology={["Node", "Express", "React", "MYSQL"]}
+        code="https://something.com"
+        live="s"
+        detail="/work/dummy"
+      />
     </div>
-}
+  );
+};
 
-export default Work
+export default Work;
