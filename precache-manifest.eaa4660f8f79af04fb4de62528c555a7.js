e45f6825fe9c30553fac49cc4c141656c5e0b160
sandip satyal <sandipsatyal25@gmail.com>
2020-08-28 00:42:55 +0545

    Updates

---
+++ precache-manifest.eaa4660f8f79af04fb4de62528c555a7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473599c3246888cc0e9bc8b95281e429",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4fa7ebda04c6e2a8e4c1",
+    "url": "/portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "50a392e7c6844aa1b2cc",
+    "url": "/portfolio/static/css/main.e813ec5b.chunk.css"
+  },
+  {
+    "revision": "4fa7ebda04c6e2a8e4c1",
+    "url": "/portfolio/static/js/2.b4717182.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.b4717182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a392e7c6844aa1b2cc",
+    "url": "/portfolio/static/js/main.a931b1bc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "39c6088edd6a679cf84af7a9d9440cfb",
+    "url": "/portfolio/static/media/DoHyeon-Regular.39c6088e.ttf"
+  },
+  {
+    "revision": "54b0af01f3ccebb6830d3985b4f0cd04",
+    "url": "/portfolio/static/media/profile.54b0af01.png"
+  }
+]);
