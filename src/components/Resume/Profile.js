3d2c8e27d4bd077c75177022a6920081fa2cf340
sandip satyal <sandipsatyal25@gmail.com>
2020-08-30 16:24:13 +0545

    [add] margin bottom

--- src/components/Resume/Profile.js
+++ src/components/Resume/Profile.js
@@ -2,13 +2,23 @@ import React from "react";
 import profileImage from "../../asset/profile.png";
 const Profile = () => {
   return (
-    <div className="text-light my-4">
+    <div className="text-light my-5">
       <div className="d-flex justify-content-center">
         <img src={profileImage} className="img img-responsive profile-image" />
       </div>
       <div className="d-flex flex-column align-items-center my-3">
-        <a href="#">Visit my linked in</a>
-        <a href="#">Download resume in pdf format</a>
+        <a
+          href="#"
+          className="text-decoration-none text-warning text-uppercase"
+        >
+          Visit my linked in profile
+        </a>
+        <a
+          href="#"
+          className="text-decoration-none text-warning text-uppercase"
+        >
+          Download resume in pdf format
+        </a>
       </div>
       <p className="font-weight-lighter">
         Software Engineer from Kathmandu, Nepal. I love learning new things
