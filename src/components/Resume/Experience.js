c27d87a9bf3b40e71ff25485ac7dfa1fc5d64e56
sandip satyal <sandipsatyal25@gmail.com>
2020-08-30 16:17:01 +0545

    [change] make date on experience small

--- src/components/Resume/Experience.js
+++ src/components/Resume/Experience.js
@@ -4,18 +4,18 @@ const Experience = () => {
   return (
     <div className="my-5 text-light">
       <div>
-        <h5>Ktm labs - Javascript Developer</h5>
-        <p className="font-weight-lighter">August 2019 - August 2020</p>
-        <p className="font-weight-lighter">
+        <h5 className="mb-0">Ktm labs - Javascript Developer</h5>
+        <small className="font-weight-lighter">August 2019 - August 2020</small>
+        <p className="font-weight-lighter mt-3">
           After successfully completing my internship at ktm labs, i started
           working as Javascript developer and delivered an excellent result by
           completing 2 web applications.
         </p>
       </div>
       <div className="mt-5">
-        <h5>Ktm labs - Intern</h5>
-        <p className="font-weight-lighter">June 2019 - July 2019</p>
-        <p className="font-weight-lighter">
+        <h5 className="mb-0">Ktm labs - Intern</h5>
+        <small className="font-weight-lighter">June 2019 - July 2019</small>
+        <p className="font-weight-lighter mt-3">
           Enrolled as an intern at ktm labs.
         </p>
       </div>
