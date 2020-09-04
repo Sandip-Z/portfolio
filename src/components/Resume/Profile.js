96953c91c1ba8108165c7eeaac2460ce837ed94a
sandip satyal <sandipsatyal25@gmail.com>
2020-09-04 20:16:10 +0545

    [remove] download to pdf and linkedIn link

--- src/components/Resume/Profile.js
+++ src/components/Resume/Profile.js
@@ -3,24 +3,24 @@ import profileImage from "../../asset/profile.png";
 const Profile = () => {
   return (
     <div className="text-light my-5">
+      {/* <div className="d-flex align-items-center justify-content-end my-0 mr-2">
+        <a
+          href="#"
+          className="text-decoration-none text-primary text-uppercase"
+        >
+          <i className="fa fa-linkedin" />
+        </a>
+        <a
+          href="#"
+          className="text-decoration-none text-primary ml-5 text-uppercase"
+        >
+          <i className="fa fa-download" aria-hidden="true"></i>
+        </a>
+      </div> */}
       <div className="d-flex justify-content-center">
         <img src={profileImage} className="img img-responsive profile-image" />
       </div>
-      <div className="d-flex flex-column align-items-center my-3">
-        <a
-          href="#"
-          className="text-decoration-none text-warning text-uppercase"
-        >
-          Visit my linked in profile
-        </a>
-        <a
-          href="#"
-          className="text-decoration-none text-warning text-uppercase"
-        >
-          Download resume in pdf format
-        </a>
-      </div>
-      <p className="font-weight-lighter">
+      <p className="mt-3 font-weight-lighter">
         Software Engineer from Kathmandu, Nepal. I love learning new things
         because i love building new things. Hope to always remain as passionate
         as i am today while writing this.
