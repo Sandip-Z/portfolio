259b8f8da3aa476ad299aa96cd485d39d9a0c879
sandip satyal <sandipsatyal25@gmail.com>
2020-09-08 15:27:36 +0545

    [add] menu for tablet-navigation

--- src/components/TabletNav.js
+++ src/components/TabletNav.js
@@ -1,13 +1,38 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const TabletNav = () => {
   return (
-    <nav className="tablet-nav">
-      <div className="d-flex justify-content-between">
-        <p className="logo text-light">Sandip</p>
-        <div className="menu text-light">X</div>
-      </div>
-    </nav>
+    <div className="tNav">
+      <input className="tNav__checkbox" type="checkbox" id="nav-toggle" />
+      <label htmlFor="nav-toggle" className="tNav__button">
+        Menu
+      </label>
+      <div className="tNav__background">&nbsp;</div>
+      <nav className="tNav__nav">
+        <ul className="tNav__list">
+          <li className="tNav__item">
+            <Link href="#" className="tNav__link">
+              Home
+            </Link>
+          </li>
+          <li className="tNav__item">
+            <Link href="#" className="tNav__link">
+              Project
+            </Link>
+          </li>
+          <li className="tNav__item">
+            <Link href="#" className="tNav__link">
+              Resume
+            </Link>
+          </li>
+          <li className="tNav__item">
+            <Link href="#" className="tNav__link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
