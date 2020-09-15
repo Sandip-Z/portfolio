9adef649f918820c085f5f76ebc1b8e9537f6cf3
sandip satyal <sandipsatyal25@gmail.com>
2020-09-15 23:08:38 +0545

    [add] hide tabletNav on link click

--- src/components/TabletNav.js
+++ src/components/TabletNav.js
@@ -1,32 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 const TabletNav = () => {
+  const [checkboxChecked, setCheckboxChecked] = useState(false);
+
+  const toggleCheckbox = () => {
+    setCheckboxChecked(!checkboxChecked);
+  };
+
   return (
     <div className="tNav">
-      <input className="tNav__checkbox" type="checkbox" id="nav-toggle" />
-      <label htmlFor="nav-toggle" className="tNav__button">
+      <input
+        className="tNav__checkbox"
+        type="checkbox"
+        id="nav-toggle"
+        checked={checkboxChecked}
+      />
+      <label
+        htmlFor="nav-toggle"
+        className="tNav__button"
+        onClick={toggleCheckbox}
+      >
         Menu
       </label>
       <div className="tNav__background">&nbsp;</div>
       <nav className="tNav__nav">
         <ul className="tNav__list">
           <li className="tNav__item">
-            <Link href="#" className="tNav__link">
+            <Link
+              to="/"
+              className="tNav__link"
+              onClick={() => setCheckboxChecked(false)}
+            >
               Home
             </Link>
           </li>
           <li className="tNav__item">
-            <Link href="#" className="tNav__link">
+            <Link
+              to="/project"
+              className="tNav__link"
+              onClick={() => setCheckboxChecked(false)}
+            >
               Project
             </Link>
           </li>
           <li className="tNav__item">
-            <Link href="#" className="tNav__link">
+            <Link
+              to="/resume"
+              className="tNav__link"
+              onClick={() => setCheckboxChecked(false)}
+            >
               Resume
             </Link>
           </li>
           <li className="tNav__item">
-            <Link href="#" className="tNav__link">
+            <Link
+              to="/contact"
+              className="tNav__link"
+              onClick={() => setCheckboxChecked(false)}
+            >
               Contact
             </Link>
           </li>
