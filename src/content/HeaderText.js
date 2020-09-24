817cff336ee9c34ba8589ae2de2318f793dea405
sandip satyal <sandipsatyal25@gmail.com>
2020-09-24 20:16:39 +0545

    [change] my description

--- src/content/HeaderText.js
+++ src/content/HeaderText.js
@@ -2,5 +2,5 @@ export const headerTexts = {
   title: "Hello world!",
   subtitle: "I am Sandip Satyal.",
   description:
-    "I'm a frontend developer. I spend most of my time writing code for applications using Javascript, React js and a little Vue js. I use CSS and SASS to design efficient user interfaces. I love talking about tech and will be sharing some experiences on my blog",
+    "I'm a frontend developer. I spend most of my time writing code for applications using Javascript, React js and a little Node js. I use CSS and SASS to design efficient user interfaces. I love talking about tech and will be sharing some experiences on my blog soon.",
 };
