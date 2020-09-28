7636f2f0dbc8fc4a1df20fe7edd09ff235a2d19a
sandip satyal <sandipsatyal25@gmail.com>
2020-09-29 01:11:15 +0545

    [add] function to add and remove active technology

--- src/context/ActiveTechnology.context.js
+++ src/context/ActiveTechnology.context.js
@@ -1,7 +1,31 @@
 import { createContext } from "react";
 
-const activeTechnology = ["Bootstrap"];
+let activeTechnologies = ["Bootstrap"];
 
-const ActiveTechnologyContext = createContext(activeTechnology);
+const updateActiveTechnologies = (technology) => {
+  activeTechnologies.forEach((activeTech, index) => {
+    if (activeTech === technology) {
+      removeFromActiveTechnology(index);
+    } else {
+      addToActiveTechnology(technology);
+    }
+  });
+};
+
+const addToActiveTechnology = (technology) => {
+  return activeTechnologies.push(technology);
+};
+
+const removeFromActiveTechnology = (index) => {
+  const clonedActiveTechnologies = [...activeTechnologies];
+  clonedActiveTechnologies.splice(index, 1);
+  console.log(clonedActiveTechnologies);
+  activeTechnologies = clonedActiveTechnologies;
+};
+
+updateActiveTechnologies("CSS3");
+updateActiveTechnologies("CSS3");
+
+const ActiveTechnologyContext = createContext(activeTechnologies);
 
 export default ActiveTechnologyContext;
